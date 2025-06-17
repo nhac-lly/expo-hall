ea12b30483e45738fd9dbe06bf807bb1be49dbd0
john-wick <john-wick>
2025-06-17 11:06:30 +0700

    update stg8

---
+++ src/app/stg8/loading.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import { Loader } from "@react-three/drei"
+
+export default function Loading() {
+  return <Loader/>
+}
---
+++ src/app/stg8/page.tsx
@@ -0,0 +1,5 @@
+import V8 from "@/components/V8";
+
+export default function Stg8() {
+  return <V8 />;
+}
